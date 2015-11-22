27097a8e86d9f35ade4bcf0fbfcbc63657585e0b
javogt <jakobavogt@gmail.com>
2015-11-22 10:54:20 +0100

    first green test

--- custom_dynamic_objects/tests/CustomDynamicObjectsTest.php
+++ custom_dynamic_objects/tests/CustomDynamicObjectsTest.php
@@ -4,8 +4,18 @@ class StupidTest extends \PHPUnit_Framework_TestCase
 {
  
 	public function testCreateBackendAddsFieldForJsonMock(){
-		// $jsonsMock = $this->getMockBuilder('CustomDynamicObjectsJsons')->getMock();
-		$wpConnectorMock = $this->getMockBuilder('CustomDynamicObjectsWordpressConnector')->getMock();
+		$jsonsMock = $this->getMockBuilder('CustomDynamicObjectsJsons')->setConstructorArgs(array('test'))->getMock();
+		$wpConnectorMock = $this->getMockBuilder('CustomDynamicObjectsWordpressConnector')
+		 	->setMethods(array('add_action'))
+			->getMock();
+
+		$wpConnectorMock->expects($this->once())
+            ->method('add_action');
+
+		$customDynamicObjects = new CustomDynamicObjects($wpConnectorMock, $jsonsMock);
+		$customDynamicObjects->createBackend();
+
+		
 	}
 
 }
