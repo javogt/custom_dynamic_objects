9230d433c8e8fa6528c784228bebb2a279b9d880
javogt <jakobavogt@gmail.com>
2015-12-10 10:02:51 +0100

    More tests for json class

--- custom_dynamic_objects/tests/php/CustomDynamicObjects/JsonsTest.php
+++ custom_dynamic_objects/tests/php/CustomDynamicObjects/JsonsTest.php
@@ -36,22 +36,48 @@ namespace CustomDynamicObjects {
 			$this->assertNull($result);
 		}
 
+		/**
+		 * @description Dataprovider for testGetObjectTypesReturnsArrayOfAllJsonFiles
+		 */
 		public function returnsDirsAndFileArrays(){
 			return [
-				'one file' => [['hey.json'], [['file' => 'existing/path/hey.json']]]
+				'one json file' => [['hey.json'], [['file' => 'existing/path/hey.json']]],
+				'two json files' => [['hey.json', 'ho.json'], [['file' => 'existing/path/hey.json'],['file' => 'existing/path/ho.json']]],
+				'two json files and another' => [['hey.json', 'ho.json', 'nope.txt'], [['file' => 'existing/path/hey.json'],['file' => 'existing/path/ho.json']]],
 			];
 		}
 
 		/**
 		 * @dataProvider returnsDirsAndFileArrays
 		 */
-		public function testGetObjectTypesReturnsArrayOfAllFiles($dir, $expected){
+		public function testGetObjectTypesReturnsArrayOfAllJsonFiles($dir, $expected){
 			global $fileExistsMockReturn, $scandirMockReturn;
 			$fileExistsMockReturn = true;
 			$scandirMockReturn = $dir;
 			$jsons = new Jsons('existing/path');
 			$result = $jsons->getObjectTypes();
 			$this->assertEquals($expected, $result);
+		}
+
+		public function testGetObjectLabelReturnsOnlyPropertyFile(){
+			$object = ['file' => 'file'];
+			$jsons = new Jsons('');
+			$result = $jsons->getObjectLabel($object);
+			$this->assertSame('file', $result);
+		}
+
+		public function testGetObjectLabelReturnsOnlyPropertyName(){
+			$object = ['name' => 'name', 'file' => 'file'];
+			$jsons = new Jsons('');
+			$result = $jsons->getObjectLabel($object);
+			$this->assertSame('name', $result);
+		}
+
+		public function testGetObjectLabelReturnsOnlyPropertyLabel(){
+			$object = ['label' => 'label', 'name' => 'name', 'file' => 'file'];
+			$jsons = new Jsons('');
+			$result = $jsons->getObjectLabel($object);
+			$this->assertSame('label', $result);
 		}
 
 	}
